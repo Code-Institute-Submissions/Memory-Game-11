a19eba70eb154f716c78170eb8c4845d0b81694a
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-03-30 13:03:57 +0000

    new structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,97 @@
-const boxes = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'teal', 'gold'];
+$(document).ready(function() {
 
-const game = {};
-$('.btn').click(startGame);
-$('.game').on("click", ".active", function(event) {
-    console.log($(this).data('val'));
-    if (!game.pause) {
-        game.clicks++;
-        $('.score').text(game.clicks);
-        game.sel.push($(this));
-        $(this).removeClass('active');
-        $(this).find('.back').hide();
-        $(this).find('.front').show();
-        if (game.sel.length === 2) {
-            console.log(game.sel);
-            if (game.sel[0].data('val') == game.sel[1].data('val')) {
-                game.pause = false;
-                console.log('Match');
-                removeItems(game.sel[0].data('val'));
-                game.sel = [];
-                if (game.newArray.length == 0) {
-                    console.log('GAME OVER');
-                    gameOver();
+    // Global variables
+
+    const cards = ['red', 'blue', 'green', 'purple', 'yellow', 'pink', 'teal', 'gold'];
+    const game = {};
+
+    // Game Play
+    $('.startBtn').click(startGame);
+    $('#restart').click(startGame);
+    $('#restart').hide();
+
+    $('.game').on("click", ".active", function(event) {
+        if (!game.pause) {
+            game.clicks++;
+            $('#score').text(game.clicks);
+            game.sel.push($(this));
+            $(this).removeClass('active');
+            $(this).find('.back-face').hide();
+            $(this).find('.front-face').show();
+            if (game.sel.length === 2) {
+                if (game.sel[0].data('val') == game.sel[1].data('val')) {
+                    game.pause = false;
+                    removeItems(game.sel[0].data('val'));
+                    game.sel = [];
+                    if (game.newArray.length == 0) {
+                        gameOver();
+                    }
+                } else {
+                    game.pause = true;
+                    game.timer = setInterval(hideCard, 1000);
                 }
-            } else {
-                game.pause = true;
-                game.timer = setInterval(hideCard, 1000);
             }
         }
+    });
+
+    function gameOver() {
+        $('.startBtn').show();
+        $('#restart').hide();
+        $('#score').text(game.clicks + 'clicks');
     }
-})
 
-function gameOver() {
-    console.log('*********GAMEOVER*********');
-    $('.btn').show();
-    $('.score').text('GAME OVER ' + game.clicks + 'clicks');
-}
+    function removeItems(val) {
+        game.newArray = game.newArray.filter(function(ele) {
+            return ele != val;
+        })
+    }
 
-function removeItems(val) {
-    console.log(game.newArray);
-    game.newArray = game.newArray.filter(function(ele) {
-        return ele != val;
-    })
-    console.log(game.newArray);
-}
+    function hideCard() {
+        flipper(game.sel[0]);
+        flipper(game.sel[1]);
+        clearInterval(game.timer);
+        game.sel = [];
+        game.pause = false;
+    }
 
-function hideCard() {
-    console.log('no match');
-    flipper(game.sel[0]);
-    flipper(game.sel[1]);
-    clearInterval(game.timer);
-    game.sel = [];
-    game.pause = false;
-}
+    function flipper(el) {
+        el.addClass('active');
+        el.find('.back-face').show();
+        el.find('.front-face').hide();
+    }
 
-function flipper(el) {
-    el.addClass('active');
-    el.find('.back').show();
-    el.find('.front').hide();
-}
+    function arrayRandomize(arr) {
+        arr.sort(function() {
+            return .5 - Math.random();
+        })
+    }
 
-function arrayRandomize(arr) {
-    arr.sort(function() {
-        return .5 - Math.random();
-    })
-}
+    function startGame() {
+        $('.startBtn').hide();
+        $('#restart').show();
+        game.clicks = 0;
+        game.pause = false;
+        game.sel = [];
+        game.newArray = cards.concat(cards);
 
-function startGame() {
-    console.log('start');
-    $('.btn').hide();
-    game.clicks = 0;
-    game.pause = false;
-    game.sel = [];
-    game.newArray = boxes.concat(boxes);
-    console.log(game.newArray);
-    arrayRandomize(game.newArray);
-    $('.game').html('');
-    $.each(game.newArray, function(key, value) {
-        console.log(key);
-        console.log(value);
-        let box = $('<div>');
-        console.log(box);
-        box.addClass('box active');
-        box.data('cnt', key + 1);
-        box.data('val', value);
-        let back = $('<div>');
-        back.addClass('back');
-        back.html(key + 1);
-        box.append(back);
-        let front = $('<div>');
-        front.css('background-color', value);
-        front.text(value);
-        front.addClass('front');
-        box.append(front);
-        $('.game').append(box);
-    })
-}
+        arrayRandomize(game.newArray);
+        $('.game').html('');
+        $.each(game.newArray, function(key, value) {
+            let card = $('<div>');
+            card.addClass('card active');
+            card.data('cnt', '?');
+            card.data('val', value);
+            let back = $('<div>');
+            back.addClass('back-face');
+            back.html('?');
+            card.append(back);
+            let front = $('<div>');
+            front.css('background-color', value);
+            front.text(value);
+            front.addClass('front-face');
+            card.append(front);
+            $('.game').append(card);
+        })
+    }
+
+});
